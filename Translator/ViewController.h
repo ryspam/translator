e65a5b910130f258ef9bdeebd1d1307a9dc2102a
Ruslan Syanov <rus604@gmail.com>
2016-04-16 16:27:46 +0300

    Initial Commit

---
+++ Translator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Translator
+//
+//  Created by Руслан on 16.04.16.
+//  Copyright © 2016 rsianov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Translator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Translator
+//
+//  Created by Руслан on 16.04.16.
+//  Copyright © 2016 rsianov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
